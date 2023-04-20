6a139db50c39497911472b708fafbfbdaf415700
thrudr <ariel.oliveiraesilva@gmail.com>
2023-04-20 16:32:25 +0200

    main-program/superclass/book-class

---
+++ CSharp-Oop-Shop2/Book.cs
@@ -0,0 +1,27 @@
+﻿using CSharp_Shop_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Oop_Shop2
+{
+    public class Book : Article
+    {
+        //ATTRIBUTES
+        private string author;
+        private string ibns;
+
+        //CONSTRUCTOR
+        public Book(string name, string description, float price, int iva, string author, string ibns) : base(name, description, price, iva);
+
+        //GETTERS
+        public string GetAuthor() { return this.author; }
+
+        public string GetIbns() { return this.ibns; }
+
+        //METHODS
+
+    }
+}
--- CSharp-Oop-Shop2/Program.cs
+++ CSharp-Oop-Shop2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using CSharp_Shop_2;
+
+Console.WriteLine("OOP Shop");
+
+/*test
+Article shop1 = new Article("Nome", "description", 25.50f, 22);
+shop1.printArticle();
+*/
+
+Article shop1 = new Article("Candele", "Colorate - Non profumate - Set con 9 candele", 10.00f, 22);
+shop1.printArticle();
+
+Article shop2 = new Article("Libro - Nel Giardino della Strega Verde", "Arin Murphy-Hiscock", 15.00f, 22);
+shop2.printArticle();
+
+Article shop3 = new Article("Punte Naturali di Ametista", "Laser grezzo proveniente del Brasile", 15.00f, 22);
+shop3.printArticle();
